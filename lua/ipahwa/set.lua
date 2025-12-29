dc434365abd21744c3efd8c58cd90e0c083c2356
Tezzish <ishanpahwa14@gmail.com>
2025-12-29 22:12:17 +0100

    add wildmenu options

--- lua/ipahwa/set.lua
+++ lua/ipahwa/set.lua
@@ -5,9 +5,11 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
 vim.opt.scrolloff = 8
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = {"longest:full", "full"}
