492ca5fc3fac30854cb977b7917e9db34ad0fa7f
Tezzish <ishanpahwa14@gmail.com>
2026-01-03 18:22:09 +0100

    Change treesitter branch to working version

--- lua/ipahwa/plugins/treesitter.lua
+++ lua/ipahwa/plugins/treesitter.lua
@@ -1,9 +1,38 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "lua", "vim", "python", "java", "javascript", "typescript", "go" },
-    highlight = { enable = true },
-  },
-}
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+      },
+    })
+  end,
+}
