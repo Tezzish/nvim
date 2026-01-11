ad6f30eafa99435b9654b383cf4c25e38b2b1bcd
Tezzish <ishanpahwa14@gmail.com>
2026-01-11 21:13:50 +0100

    Removes netrw nav

--- lua/ipahwa/remap.lua
+++ lua/ipahwa/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", "<C-w>")
+vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Window options" })
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+
