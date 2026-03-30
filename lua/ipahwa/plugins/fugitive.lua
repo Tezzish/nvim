4eb233bf805a0e1f760e1fce74c48e9d1fe730f7
Tezzish <ishanpahwa14@gmail.com>
2026-03-30 19:41:44 +0200

    Updates config

--- lua/ipahwa/plugins/fugitive.lua
+++ lua/ipahwa/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
     'tpope/vim-fugitive',
+    config = function()
+        vim.api.nvim_create_user_command('Gcm', function(opts)
+            vim.cmd('G commit -m "' .. opts.args .. '"')
+        end, { nargs = '+' })
+    end,
 }
